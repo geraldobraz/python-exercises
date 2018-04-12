f7dd7c9ad8ebc488fd3fab1eca89e81aad4bedad
geraldobraz <gban50@gmail.com>
2018-04-12 18:51:38 -0300

    - Hamming Distance: Código finalizado!

--- Hamming Distance.py
+++ Hamming Distance.py
@@ -1,16 +1,9 @@
 # hamming_distance
-# TODO: pegar uma das strings como ref dps comparar elemento por elemento com a segunda string se for dif contar no contador
-
-s1 ="1010"
-s2 = "0101"
-
-ls1 = list(s1)
-ls2 = list(s2)
-contador = 0
-for i in ls1:
-    for x in range(0,len(ls2)):
-        if i != ls2[x]:
-            contador = 1 + contador
-        else:
-            pass
-print(contador)
+def hamming_distance(a, b):
+    listaS1 = [int(x) for x in a]
+    listaS2 = [int(y) for y in b]
+    contador = 0
+    for i in range(0,len(listaS1)):
+        if listaS1[i] != listaS2[i]:
+            contador = contador+1
+    return contador
