2d4fc13287aee6f7934ea0b6c5e378ef97682e74
geraldobraz <gban50@gmail.com>
2018-04-13 18:19:35 -0300

    - Split odd and even: Código em processo de contrução!

--- Split odd and even.py
+++ Split odd and even.py
@@ -1,32 +1,62 @@
+'''
+Test.assert_equals(split_odd_and_even(123), [1,2,3])
+Test.assert_equals(split_odd_and_even(111),  [111])
+Test.assert_equals(split_odd_and_even(223),  [22,3])
+'''
 def split_odd_and_even(n):
-     numeros = [int(i) for i in n]
-     print(numeros)
+    numeros = [int(i) for i in n]
+    print(numeros)
 
 
 even = []
 ods = []
-saida =[]
-numeros = [int(i) for i in "112"]
-print(numeros)
-validador = False
-
+saida = []
+numeros = [int(i) for i in "113"]
+# print(len(numeros))
 for x in range(0,len(numeros)):
-    print(numeros[x])
-    if (numeros[x]%2) == 0:
-        # print(numeros[x+1])
-        if numeros[x+1]%2 ==0:
-            even.append(numeros[x])
-            validador = True
-        else:
-            if validador:
-                saida.append(even)
-                validador = False
-            else:
-                saida.append(numeros[x])
-    else:
-       saida.append(numeros[x])
 
-print(saida)
+    if numeros[x]%2 == 0:
+        print("Even",numeros[x])
+
+
+    else:
+        print("Ods",numeros[x])
+        if (x+1) < len(numeros):
+            if numeros[x+1]%2 != 0:
+                print("Prox tmb n e' par")
+                ods.append(numeros[x])
+        else:
+            print("NAO ok")
+print(ods)
+
+
+
+
+
+
+
+
+
+
+
+
+# for x in range(0,len(numeros)):
+#     print(numeros[x])
+#     if (numeros[x]%2) == 0:
+#         # print(numeros[x+1])
+#         if numeros[x+1]%2 ==0:
+#             even.append(numeros[x])
+#             validador = True
+#         else:
+#             if validador:
+#                 saida.append(even)
+#                 validador = False
+#             else:
+#                 saida.append(numeros[x])
+#     else:
+#         saida.append(numeros[x])
+#
+# print(saida)
     #     if (x+1)%2 == 0:
     #         even.append(x)
     #     else:
