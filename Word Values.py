c93b33adfb25e49d6941aac279b1e509cb35300e
geraldobraz <gban50@gmail.com>
2018-04-14 16:45:18 -0300

    - Word Values: Código em Finalizado e publicado!

--- Word Values.py
+++ Word Values.py
@@ -1,17 +1,16 @@
 # Código Word Values
-# Disciplina de Python
-# __author__ : Geraldo Braz
 import string
-
 def name_value(my_list):
-    teste = {(list(string.ascii_lowercase)[i]): (i+1) for i in range(0, 26)}
-    str(my_list).replace(" ", "")
-    soma = 0
-    for a in my_list:
-        soma = teste[a] + soma
-    return soma
+    resp = []
+    for a in range(0,len(my_list)):
+        teste = {(list(string.ascii_lowercase)[i]): (i+1) for i in range(0, 26)}
+        my_list[a]= str(my_list[a]).replace(" ", "")
+        soma = 0
+        for i in my_list[a]:
+            soma = teste[i] + soma
+        resp.append(soma*(a+1))
+    return resp
 
-stList = ["codewars","ada","xyz"]
+stList = ["abc abc","abc abc","abc","abc"]
 
-for a in range(0,len(stList)):
-    print((name_value(stList[a]))*(a+1))
+print(name_value(stList))
