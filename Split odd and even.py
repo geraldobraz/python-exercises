bf0279a98debfb688dc021f8caa90c5c629a178f
geraldobraz <gban50@gmail.com>
2018-04-15 20:43:29 -0300

    - Split odd and even: Código em Finalizado e publicado!

--- Split odd and even.py
+++ Split odd and even.py
@@ -1,74 +1,38 @@
-'''
-Test.assert_equals(split_odd_and_even(123), [1,2,3])
-Test.assert_equals(split_odd_and_even(111),  [111])
-Test.assert_equals(split_odd_and_even(223),  [22,3])
-'''
+def isEven(n):
+    return True if int(n)%2 == 0 else False
 def split_odd_and_even(n):
-    numeros = [int(i) for i in n]
-    print(numeros)
 
-
-even = []
-ods = []
-saida = []
-numeros = [int(i) for i in "113"]
-# print(len(numeros))
-for x in range(0,len(numeros)):
-
-    if numeros[x]%2 == 0:
-        print("Even",numeros[x])
-
-
-    else:
-        print("Ods",numeros[x])
-        if (x+1) < len(numeros):
-            if numeros[x+1]%2 != 0:
-                print("Prox tmb n e' par")
-                ods.append(numeros[x])
+    numeros = str(n)
+    size = 0
+    last = int(numeros[0])
+    i = 1
+    out = []
+    tamanho_numeros = len(numeros)
+    while size < tamanho_numeros:
+        num = int(numeros[:i])
+        if isEven(num):
+            if not isEven(last):
+                out.append(last)
+                size += len(str(last))
+                numeros = numeros[i - 1:]
+                i = 1
+            else:
+                i += 1
         else:
-            print("NAO ok")
-print(ods)
+            if isEven(last):
+                out.append(last)
+                size += len(str(last))
+                numeros = numeros[i - 1:]
+                i = 1
+            else:
+                i += 1
+
+        if i > len(numeros[:i]):
+            out.append(num)
+            break
+
+        last = num
+    return out
 
 
 
-
-
-
-
-
-
-
-
-
-# for x in range(0,len(numeros)):
-#     print(numeros[x])
-#     if (numeros[x]%2) == 0:
-#         # print(numeros[x+1])
-#         if numeros[x+1]%2 ==0:
-#             even.append(numeros[x])
-#             validador = True
-#         else:
-#             if validador:
-#                 saida.append(even)
-#                 validador = False
-#             else:
-#                 saida.append(numeros[x])
-#     else:
-#         saida.append(numeros[x])
-#
-# print(saida)
-    #     if (x+1)%2 == 0:
-    #         even.append(x)
-    #     else:
-    #         pass
-    # else:
-    #     pass
-        # print("Ods")
-        # ods.append(x)
-
-
-# even = ''.join(str(e) for e in even)
-# ods = ''.join(str(e) for e in ods)
-#
-# saida.append(ods)
-# saida.append(even)
